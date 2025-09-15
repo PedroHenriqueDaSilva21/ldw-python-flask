27bcf520b5d1e102d068ca7cb7bc0bc23df5d479
DSM4 <fatec-dsm4@academico.intra>
2025-09-15 18:33:01 -0300

    :wrench:fix:ajuste da página de renderização de paises na api

--- atividade2/controllers/routes.py
+++ atividade2/controllers/routes.py
@@ -13,7 +13,7 @@ def init_app(app: Flask):
 
     @app.route('/')
     def home():
-        return render_template('home.html')
+        return render_template('base.html')
 
     @app.route('/destinos', methods=['GET', 'POST'])
     def destinos_page():
